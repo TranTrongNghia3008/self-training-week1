d092979adfeb28a40641d4687ecbe0a1006e7415
Phuoc Nguyen <phuoc.nguyenhoang@hdwebsoft.dev>
2025-07-21 14:17:24 +0700

    Add Contact and Group models with relationships

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField(blank=True)
+    groups = models.ManyToManyField(Group, related_name="contacts")
+
+    def __str__(self):
+        return self.name
